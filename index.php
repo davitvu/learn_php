873c5dd09e44826e03f8f4c7f223b98655f314d0
Vu Xuan Truong <101324265+davitvu@users.noreply.github.com>
2024-10-28 22:50:14 +0700

    tao repo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang PHP Đơn Giản</title>
+</head>
+<body>
+    <h1>Chào mừng đến với trang PHP của tôi!</h1>
+    <?php
+        echo "<p>Đây là đoạn văn bản được tạo bởi PHP.</p>";
+        
+        echo "<p>Thời gian hiện tại là: " . date("H:i:s d-m-Y") . "</p>";
+    ?>
+</body>
+</html>
