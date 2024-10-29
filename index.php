96454e3d4596a8264974af647f41b9ef554f6c9c
Vu Xuan Truong <101324265+davitvu@users.noreply.github.com>
2024-10-29 23:25:00 +0700

    ok

--- index.php
+++ index.php
@@ -22,6 +22,21 @@
         if($c == 3) echo "<p>giong $c</p>";
         elseif($c == $b) echo "<p>khac $c = $b</p>";
         else echo "<p>khong giong cai nao</p>";
+
+        // đếm ký tự
+        echo strlen("Ần") . "<br>"; // An = 2; Ân = 3; Ần = 4;
+
+        // đếm số lượng từ
+        echo str_word_count("trặt") . "<br>";
+
+        // đảo ngược chuỗi
+        echo strrev("Nguyen ronal do") . "<br>";
+
+        // tìm giá trị trả về index
+        echo strpos("123aa", "3") . "<br>";
+
+        // thay chuoi Cú pháp => (chuỗi, từ muốn thay, chuỗi, từ cần thay, "chuỗi gốc")
+        echo str_replace("nguyen ro", "tran va", "nguyen ronal do") . "<br>";
     ?>
 </body>
 </html>
